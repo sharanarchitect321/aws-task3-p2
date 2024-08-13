5beaa181af7ce0c663b312d90670f6202e972046
shravan325 <113554880+shravan325@users.noreply.github.com>
2024-08-13 19:07:42 -0400

    test01

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='my-python-lambda-app',
+    version='0.1',
+    description='A simple Python Lambda application',
+    packages=['src'],
+    install_requires=[
+        # List your dependencies here
+    ],
+)
